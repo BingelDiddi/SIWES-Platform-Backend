a0dae679486b1d1d0c84051bb88b1e8c89387e9b
Fhadiil <fadeelaliyu51@gmail.com>
2025-11-27 21:09:59 +0100

    added matric field tolog serializer

--- logbooks/serializers.py
+++ logbooks/serializers.py
@@ -3,9 +3,10 @@ from .models import LogEntry, FinalReport
 
 class LogEntrySerializer(serializers.ModelSerializer):
     student_name = serializers.SerializerMethodField()
+    matric = serializers.CharField(source='student.student_profile.matric_number', read_only=True)
     class Meta:
         model = LogEntry
-        fields = ['id', 'date', 'time_in', 'time_out', 'activities', 'status', 'supervisor_feedback', 'student_name']
+        fields = ['id', 'date', 'time_in', 'time_out', 'activities', 'status', 'supervisor_feedback', 'student_name', 'matric']
         read_only_fields = ['status', 'supervisor_feedback'] # Students can't edit status
     def get_student_name(self, obj):
         return f"{obj.student.first_name} {obj.student.last_name}"
